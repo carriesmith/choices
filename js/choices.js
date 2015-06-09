5398f234ae2d5251adef9e745e57d28338113866
carriesmith <carrie.elizabeth@gmail.com>
2015-06-09 19:38:42 -0400

    generalize item list js

--- js/choices.js
+++ js/choices.js
@@ -1,60 +1,67 @@
 var allOptions = [];
+var allValues = [];
 
-function createChoiceItem() {
-}
+function addItem(array, inputText){
+  if ( inputText !== ""){
 
-function addChoiceItem(){
-  if ($('input[name="item"]').val() !== ""){
-    allOptions.push($('input[name="item"]').val());
-    console.log(allOptions);
-    drawChoiceItems(allOptions);
+    // Look for matching text
+    var indexMatch = $.map(array, function(value){return value.toLowerCase()}).indexOf(inputText.toLowerCase());
+
+    if( indexMatch === -1){
+       array.unshift(inputText);      
+    } else {
+      array.splice(indexMatch, 1);
+      array.unshift(inputText)
+    }
+
   }
-  $('input[name="item"]').val("");
+  return array;
 }
 
-function drawChoiceItems(allOptions){
-  // do not add if hit submit / enter on null string
-  var taskText = $('input[name="item"]').val();
+function drawItems(whichList, array){
   
-  $('.items li').remove();
+  console.log(whichList + ' .items li');
+  $(whichList + ' .items li').remove();
 
-  for (var iChoice in allOptions){
-    console.log(allOptions[iChoice]);
-    var li = '<li id="i-' + iChoice + '"><div class="input-group"> <div class="form-control form-control-static option-item" aria-describedby="basic-addon1">' + allOptions[iChoice] + '</div><span class="input-group-btn"><button class="btn btn-default go-away-button" type="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></span></span></div></li>';
-    $('.items').append(li);
+  for (var i in array){
+    var li = '<li id="i-' + i + '"><div class="input-group"> <div class="form-control form-control-static option-item" aria-describedby="basic-addon1">' + array[i] + '</div><span class="input-group-btn"><button class="btn btn-default go-away-button" type="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></span></span></div></li>';
+    $(whichList + ' .items').append(li);
   }
 
 }
-
-// function getVals(selector) {
-//   return $(selector).map(function() {
-//           return($(this).text());
-//       }).get();
-// }
-
 
 $(function(){
-  // listen for when someone adds a todo item
+  // listen for when someone adds an item
   // when someone adds one, get the text out of the input, and put it into the ul
 
-  $('.todo').on('submit', function(event){ 
-    
+  $('.option-form').on('submit', function(event){ 
     // prevent default form behaviour refresh page
     event.preventDefault();
-    addChoiceItem();
-
+    var inputText = $('.option-form input[name="item"]').val();
+    allOptions = addItem(allOptions, inputText);
+    drawItems('.options-list', allOptions);
+    $('.option-form input[name="item"]').val("");
   });
 
-  $('#go-button').on('click', function(event){ 
-    addChoiceItem();
+  $('.option-form #go-button').on('click', function(event){ 
+    var inputText = $('.option-form input[name="item"]').val();
+    allOptions = addItem(allOptions, inputText);
+    drawItems('.options-list', allOptions);
+    $('.option-form input[name="item"]').val("");
   });
 
   // Append listener to li children of ul
   // EVENT DELEGATION to all future children of ul.items
 
-  $('ul.items').on('click', '.go-away-button', function(){
-    console.log($(this).prev('.option-item'));
-    //$(this).parents('li').remove();
+  $('.options-list ul.items').on('click', '.go-away-button', function(){
+    var textToRemove = $(this).parent().prev().text();
+
+    // remove from the data array
+    var index = allOptions.indexOf(textToRemove); 
+    if (index !== -1) {
+        allOptions.splice(index, 1);
+    }
+    drawItems('.options-list', allOptions);
   });
 
   // make them sortable!! whoa.
@@ -63,7 +70,6 @@ $(function(){
   $('ul.items').sortable({
       update: function (event, ui) {
           var newOrder = $(this).sortable('toArray');
-          console.log(newOrder);
           // conver to numeric
           newOrder = newOrder.map(function(idText){
             return parseInt(idText.replace('i-',''));
@@ -75,8 +81,8 @@ $(function(){
           }
           allOptions = temp;
           drawChoiceItems(allOptions);
+          console.log(allOptions)
       }
   });
-
 
 });
